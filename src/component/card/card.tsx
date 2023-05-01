3b80f80afa750d6bd8af76beef8881b28260cbcb
julio-valim <juliopvalim@gmail.com>
2023-05-01 19:12:34 -0300

    improving card presentation

--- src/component/card/card.tsx
+++ src/component/card/card.tsx
@@ -1,17 +1,20 @@
 import "./card.css"
 
-interface CardProps{
+interface CardProps {
     title: string,
     cover: string,
     price: number
 }
 
-export function Card( {title, cover, price}: CardProps){
-    return(
+export function Card({ title, cover, price }: CardProps) {
+    return (
         <div className="card">
-            <img/>
-            <h2></h2>
-            <p></p>
+            <img src={cover} />
+            <h2>{title}</h2>
+            <p className="card-price">{Intl.NumberFormat('pt-br', {
+                style: 'currency',
+                currency: 'BRL',
+            }).format(price)}</p>
         </div>
     )
 }
