75a79bec0df0df2d9f54e53e245b4bb6ece9c4f4
julio-valim <juliopvalim@gmail.com>
2023-05-01 14:21:32 -0300

    add data validation; update couseData object;

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 // import { useState } from 'react'
 import { Card } from './component/card/card'
-import { CourseData } from './interface/CoursseData'
+import { useCourseData } from './hooks/useCourseData'
+import { CourseData } from './interface/CourseData'
 
 function App() {
   // const [count, setCount] = useState(0)
-  const data: CourseData[] = []
+  const { data } = useCourseData();
 
   return (
     <div className='container'>
       <h1>Cursos</h1>
       <div className="card-grid">
-        {data.map(courseData =>
+        {data?.map(courseData =>
           <Card
             title={courseData.title}
             cover={courseData.cover}
